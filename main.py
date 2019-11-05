4420019aa5542f11444e954440c7b9241eb7babf
Saad <daasdaham@gmail.com>
2019-11-05 21:57:08 +0530

    mean corrected

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import seaborn as sns
+#import seaborn as sns
 class Model:
 
 	def __init__(self,dataset):
@@ -10,16 +10,13 @@ class Model:
 		print(self._data.head())
 
 	def addMean(self):
-		self._data['A_mean'] = self._data[['A_1','A_2','A_3','A_3']].mean(axis=1)
-		self._data['B_mean'] = self._data[['B_1','B_2','B_3','B_3']].mean(axis=1)
-		self._data['C_mean'] = self._data[['C_1','C_2','C_3','C_3']].mean(axis=1)
-		self._data['D_mean'] = self._data[['D_1','D_2','D_3','D_3']].mean(axis=1)
+		self._data['mean_per_gene'] = self._data[['A_1','A_2','A_3','B_1','B_2','B_3','C_1','C_2','C_3','D_1','D_2','D_3']].mean(axis=1)
 
 	def addSD(self):
-		self._data['A_SD'] = self._data[['A_1','A_2','A_3','A_3']].std(axis=1)
-		self._data['B_SD'] = self._data[['B_1','B_2','B_3','B_3']].std(axis=1)
-		self._data['C_SD'] = self._data[['C_1','C_2','C_3','C_3']].std(axis=1)
-		self._data['D_SD'] = self._data[['D_1','D_2','D_3','D_3']].std(axis=1)
+		self._data['A_SD'] = self._data[['A_1','A_2','A_3']].std(axis=1)
+		self._data['B_SD'] = self._data[['B_1','B_2','B_3']].std(axis=1)
+		self._data['C_SD'] = self._data[['C_1','C_2','C_3']].std(axis=1)
+		self._data['D_SD'] = self._data[['D_1','D_2','D_3']].std(axis=1)
 
 	# def addIQR(self):
 	# 	self._data['A_IQR'] = self._data[['A_1','A_2','A_3','A_4']].std(axis=1)
@@ -34,10 +31,10 @@ class Model:
 		self._data['D_Z'] = (self._data['D_normalize'] - self._data['D_normalize'].mean())/self._data['D_normalize'].std()
 
 	def normalize(self):
-		self._data['A_normalize'] = self._data['A_SD']/self._data['A_mean']
-		self._data['B_normalize'] = self._data['B_SD']/self._data['B_mean']
-		self._data['C_normalize'] = self._data['C_SD']/self._data['C_mean']
-		self._data['D_normalize'] = self._data['D_SD']/self._data['D_mean']
+		self._data['A_normalize'] = self._data['A_SD']/self._data['mean_per_gene']
+		self._data['B_normalize'] = self._data['B_SD']/self._data['mean_per_gene']
+		self._data['C_normalize'] = self._data['C_SD']/self._data['mean_per_gene']
+		self._data['D_normalize'] = self._data['D_SD']/self._data['mean_per_gene']
 
 	def order(self):
 		a_var = self._data['A_normalize'].mean()
@@ -57,7 +54,7 @@ class Model:
 				position.append("C")
 			else:
 				position.append("D")
-		print(position)q
+		print(position)
 
 	def outlier_Z(self,col):
 		if(col['A_Z'] < 3 and col['A_Z'] > -3):
@@ -98,7 +95,7 @@ model.normalize()
 
 model.addZscore()
 
-#model.order()
+model.order()
 
 model.add_outlier_Z()
 

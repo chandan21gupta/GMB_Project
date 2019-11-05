e63478d75670294aa7944127cb2ecb03229e458b
Guardianofgotham <43683678+Guardianofgotham@users.noreply.github.com>
2019-11-05 22:43:44 +0530

    Add files via upload

---
+++ normalizeData.py
@@ -0,0 +1,46 @@
+import csv
+import pandas as pd
+import numpy as np
+
+file = open(".\\data.csv")
+data = pd.read_csv(file, sep=',', header=None)
+data = data.values
+numbersOnly = data[1:, 1:].astype(np.float)
+
+# dividing datasets into groups
+dataSetA = numbersOnly[:, 0:3]
+dataSetD = numbersOnly[:, 9:]
+
+# calculating standard deviatrions for each group
+standardDeviationA = np.std(dataSetA, axis=1)
+standardDeviationD = np.std(dataSetD, axis=1)
+
+# ignoring/suppressing division be zero warning/exception/error
+np.seterr(divide='ignore', invalid='ignore')
+
+# calculating mean for each group
+totalDataSet = np.concatenate((dataSetA, dataSetD), axis=1)
+totalMean = np.mean(totalDataSet, axis=1)
+print(totalMean)
+
+# normalizing standard deviation by totalMean
+normalizedA = np.divide(standardDeviationA, totalMean)
+normalizedD = np.divide(standardDeviationD, totalMean)
+
+print(normalizedA, normalizedD)
+
+# reached end of file, freeing up memory by removing reference to structures
+data = None
+numbersOnly = None
+dataSetA = None
+dataSetB = None
+dataSetC = None
+dataSetD = None
+standardDeviationA = None
+standardDeviationB = None
+standardDeviationC = None
+standardDeviationD = None
+meanA = None
+meanB = None
+meanC = None
+meanD = None
